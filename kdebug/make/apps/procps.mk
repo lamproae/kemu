1ea7d51f9073ebdd213984f8c29f7905623e3b30
Jonathan Lee <kkkmmu@163.com>
2016-06-16 10:13:15 +0800

    app procps

---
+++ kdebug/make/apps/procps.mk
@@ -0,0 +1,29 @@
+SOURCE=$(APPS_DIR)/procps/procps-$(VERSION)
+VERSION=3.2.8
+
+all: build install 
+
+CFLAGS+=-I$(APPS_DIR)/ncurses/ncurses-5.4 -I$(APPS_DIR)/ncurses/ncurses-5.4/include
+build:  $(SOURCE)/curses.h
+	@cd $(SOURCE) && make 
+
+$(SOURCE)/curses.h:
+	ln -sf $(APPS_DIR)/ncurses/ncurses-5.4/include/ncurses.h $(SOURCE)/curses.h 
+
+config:
+	@if [ ! -f $(SOURCE)/Makefile ]; then \
+	    cd $(SOURCE) &&  ./configure --host=$(ARCH) CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)"  --with-ncurses-dir=$(APPS_DIR)/ncurses/ncurses-5.4;   \
+	fi
+
+
+install:
+	find $(SOURCE)/ps/ -perm 775 -a ! -name ".deps" -a ! -type d | xargs -i $(INSTALL) {} $(ROOT_DIR)/bin/
+	find $(SOURCE)/proc/ -perm 775 -a ! -name ".deps" -a ! -type d | xargs -i $(INSTALL) {} $(ROOT_DIR)/lib/
+
+clean:
+	@cd $(SOURCE) && make clean
+
+distclean:
+	@cd $(SOURCE) && make distclean
+
+.PHONY: config build clean install

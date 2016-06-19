b8698312f160c09770efa4b33e4e55e58da729fb
Jonathan Lee <kkkmmu@163.com>
2016-06-19 16:03:20 +0800

    Fix build directy change bug

--- kdebug/script/boot.sh
+++ kdebug/script/boot.sh
@@ -6,18 +6,18 @@ if [ $# != 1 ]; then
 fi
 
 if [ "$ARCH"x = "arm"x ]; then
-    sudo qemu-system-arm -M versatilepb -m 1024 -kernel build/arch/arm/boot/zImage  
+    sudo qemu-system-arm -M versatilepb -m 1024 -kernel result/build/arch/arm/boot/zImage  
 elif [ "$ARCH"x = "x86_64"x ]; then
     if [ $1 = "host1" ]; then
-        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=1 --net tap,ifname=tap1,vlan=1,script=no,downscript=no -kernel build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
+        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=1 --net tap,ifname=tap1,vlan=1,script=no,downscript=no -kernel result/build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
     elif [ $1 = "host2" ]; then
-        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2 -net tap,ifname=tap2,vlan=2,script=no,downscript=no -kernel build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
+        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2 -net tap,ifname=tap2,vlan=2,script=no,downscript=no -kernel result/build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
     elif [ $1 = "router" ]; then
-        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=1 -net tap,ifname=tap3,vlan=1,script=no,downscript=no  -net nic,model=e1000,vlan=2 -net tap,ifname=tap4,vlan=2,script=no,downscript=no  -kernel build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
+        sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=1 -net tap,ifname=tap3,vlan=1,script=no,downscript=no  -net nic,model=e1000,vlan=2 -net tap,ifname=tap4,vlan=2,script=no,downscript=no  -kernel result/build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware &
     else
         echo "Unsupported target: $1 "
         exit -1
-        # sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2, -net tap,ifname=tap4,vlan=2 -kernel build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware
+        # sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2, -net tap,ifname=tap4,vlan=2 -kernel result/build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware
     fi
 else
     echo "Unsupported target platform"
--- kdebug/script/build-sample-os.sh
+++ kdebug/script/build-sample-os.sh
@@ -5,20 +5,20 @@ if [ $# != 1 ]; then
 fi
 
 if [ $1 = "host1" ]; then
-    rm -rf ../build/usr/initramfs_data.cpio.gz
+    rm -rf ../result/build/usr/initramfs_data.cpio.gz
     cp -a ../samples/topology/start-host1 ../rootfs/etc/init.d/rcS
     cd .. &&  make kernel
 elif [ $1 = "host2" ]; then
-    rm -rf ../build/usr/initramfs_data.cpio.gz
+    rm -rf ../result/build/usr/initramfs_data.cpio.gz
     cp -a ../samples/topology/start-host2 ../rootfs/etc/init.d/rcS
     cd .. &&  make kernel
 elif [ $1 = "router" ]; then
-    rm -rf ../build/usr/initramfs_data.cpio.gz
+    rm -rf ../result/build/usr/initramfs_data.cpio.gz
     cp -a ../samples/topology/start-router ../rootfs/etc/init.d/rcS
     cd .. &&  make kernel
 else
     echo "Unsupported target: $1 "
     exit -1
-    # sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2, -net tap,ifname=tap4,vlan=2 -kernel build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware
+    # sudo qemu-system-x86_64 -m 2000 -net nic,model=e1000,vlan=2, -net tap,ifname=tap4,vlan=2 -kernel result/build/arch/x86/boot/bzImage -append vga=0x380 -vga vmware
 fi
 
